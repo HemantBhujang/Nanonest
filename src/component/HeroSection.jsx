90243732ed2fce00a4e9e5b2e8737bd986606551
sakshi Kakde <sakshi.kakde2004@gmail.com>
2024-09-14 17:20:36 +0530

    heading and hero

---
+++ src/component/HeroSection.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import hero from "../assets/pagal sakshi.png";
+
+
+const HeroSection = () => {
+  return (
+    <div>
+      <div className="container text-center">
+  <div className="row ">
+    <div className="col my-4">
+     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita distinctio eligendi, unde fugit nisi aut aliquid aliquam labore et quas, atque a ipsa exercitationem placeat, quos ipsam culpa deserunt minima.
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi error, omnis similique sequi, itaque ullam blanditiis id earum hic laboriosam reprehenderit ducimus consequuntur veritatis totam quisquam illo dignissimos mollitia. Est.
+     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus omnis a ducimus ipsam quibusdam dicta ipsum repudiandae. Molestias, nihil reprehenderit odit consequatur neque quas sit nulla amet recusandae nesciunt eveniet?Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ducimus tempora distinctio molestiae error ullam voluptas. Quia corrupti vel velit adipisci fugiat facilis quasi recusandae tempore animi nobis, iure voluptas.
+    </div>
+    <div className="col my-4 container">
+   <img src={hero} alt="hero" width="735rem" height='533rem' className='img-thumbnail' />
+    </div>
+  </div>
+  </div>
+    </div>
+  )
+}
+
+export default HeroSection
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import logo from "../assets/react.svg";
 
-function Navbar() {
+
+
+const Navbar = ({title,services,industry,about,team,more,faq,in_faq,blog}) => {
   return (
     <div>
-      
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+  <a className="navbar-brand" href="#">
+ 
+      <img src={logo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top mx-2"></img>
+      {title}
+    </a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active mx-3" aria-current="page" href="#">{services}</a>
+        </li>
+        <li className="nav-item">
+      <a className="nav-link active mx-3 " aria-current="page" href="#">{industry}</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active mx-3" aria-current="page" href="#">{about}</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active mx-3" aria-current="page" href="#">{team}</a>
+        </li>
+        <li className="nav-item dropdown mx-3">
+          <a className="nav-link dropdown-toggle mx-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            {more}
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">{faq}</a></li>
+            <li><a className="dropdown-item" href="#">{in_faq}</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">{blog}</a></li>
+          </ul>
+        </li>
+    </ul>
+
+    <button type="button" class="btn btn-outline-warning mx-3">Contact Us </button>
+    </div>
+  </div>
+</nav>
     </div>
   )
 }
