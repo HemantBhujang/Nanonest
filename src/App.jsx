304fb71b244b5ae98750ba26850f818a69b69852
Hemant Bhujang <mrhemant619@gmail.com>
2024-09-16 18:50:20 +0530

    changes are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './component/Navbar'
 import HeroSection from './component/HeroSection'
+import Section2 from './component/Section2'
+import Apointment from './component/Apointment'
 
 const App = () => {
   return (
     <div>
       <Navbar title='NanoNest' services='Services' industry='Industry' about='About Us'  team ='Team NanoNest' more='More' faq='Founder FAQ' in_faq='Investor FAQ' blog='Blog'/>
       <HeroSection/>
+      <Section2 />
+      <Apointment />
     </div>
   )
 }
---
+++ src/component/Section2.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Section2.css'
+const Section2 = () => {
+  return (
+    <div className="how-it-works">
+      <h1>How it works</h1>
+      <div className="steps">
+        <div className="step">
+          <h2>01</h2>
+          <h3>Goal</h3>
+          <p>
+            To create a seamless connection between mission and
+            more entrepreneurs and potential investors or clients,
+            facilitating effective communication and collaboration.
+          </p>
+        </div>
+        <div className="step">
+          <h2>02</h2>
+          <h3>Competitors</h3>
+          <p>
+            Everybody wants to be the best in their field. To
+            succeed, you will want to gather information and find
+            out what others are doing so that you can take risks
+            and focus on your niche.
+          </p>
+        </div>
+        <div className="step">
+          <h2>03</h2>
+          <h3>Strategy</h3>
+          <p>
+            Focus on creating an inclusive and engaging
+            platform that facilitates the connections and
+            networks required to encourage active participation
+            and collaboration.
+          </p>
+        </div>
+        <div className="step">
+          <h2>04</h2>
+          <h3>Launch</h3>
+          <p>
+            Launching everything into action will be the start of
+            your new marketing strategy. This is where you can
+            really start to see your business grow.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section2;
