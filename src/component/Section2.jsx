35153013b5c9b10aff4f647defa06b1b3a39bd6a
sakshi Kakde <sakshi.kakde2004@gmail.com>
2024-09-17 18:46:27 +0530

    Section2 modified

--- src/component/Section2.jsx
+++ src/component/Section2.jsx
@@ -1,51 +1,23 @@
-import React from 'react';
-import './Section2.css'
-const Section2 = () => {
+import React from 'react'
+import Testonomial from '../assets/Testimonial.png'
+import GetStarted from '../assets/Get Started.png'
+import Setting from '../assets/Settings.png'
+import TeamWork from '../assets/Team Work.png'
+import Card from './Card'
+import './HeroSection.css'
+function Section2() {
   return (
-    <div className="how-it-works">
-      <h1>How it works</h1>
-      <div className="steps">
-        <div className="step">
-          <h2>01</h2>
-          <h3>Goal</h3>
-          <p>
-            To create a seamless connection between mission and
-            more entrepreneurs and potential investors or clients,
-            facilitating effective communication and collaboration.
-          </p>
-        </div>
-        <div className="step">
-          <h2>02</h2>
-          <h3>Competitors</h3>
-          <p>
-            Everybody wants to be the best in their field. To
-            succeed, you will want to gather information and find
-            out what others are doing so that you can take risks
-            and focus on your niche.
-          </p>
-        </div>
-        <div className="step">
-          <h2>03</h2>
-          <h3>Strategy</h3>
-          <p>
-            Focus on creating an inclusive and engaging
-            platform that facilitates the connections and
-            networks required to encourage active participation
-            and collaboration.
-          </p>
-        </div>
-        <div className="step">
-          <h2>04</h2>
-          <h3>Launch</h3>
-          <p>
-            Launching everything into action will be the start of
-            your new marketing strategy. This is where you can
-            really start to see your business grow.
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-};
+    <div className='container'>
+      <h1 className="text-center fw-bold my-5">How It Works </h1>
+   <Card Testonomial={Testonomial} Title='Goal' Content='To create a seamless connection between micro and nano entrepreneurs and potential investors or donors, facilitating effective communication and collaboration.' />
 
-export default Section2;
+   <Card style={{ marginLeft: '50px' }} Testonomial={GetStarted} Title='Competitors' Content='Everybody wants to be the best in their field. To achieve success, we audit your competitors and find out what is working and what is not. All this to help you save time, money, and effort on things that do not work and focus on what does.' />
+
+   <Card Testonomial={Setting} Title='Strategy' Content='Focus on creating an intuitive and engaging platform that simplifies the connection and communication process between entrepreneurs and investors, ensuring a seamless experience that encourages active participation and collaboration.'/>
+
+   <Card style={{ marginLeft: '200px' }}  Testonomial={TeamWork} Title='Launch' Content='Launching everything into action will be the start of your new marketing strategy. Taking every step we have planned and carefully following the laid-out road map to reach your business goals.​' />
+    </div>
+  )
+}
+
+export default Section2
