29b5f86dae12f2a8f1806fffb775be5560b16e82
sakshi Kakde <sakshi.kakde2004@gmail.com>
2024-09-17 18:10:27 +0530

    to pull

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -8,11 +8,7 @@ const HeroSection = () => {
     <div>
       <div className="container text-center">
   <div className="row ">
-    <div className="col mt-5 mb-0 pt-5 ">
-     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita distinctio eligendi, unde fugit nisi aut aliquid aliquam labore et quas, atque a ipsa exercitationem placeat, quos ipsam culpa deserunt minima.
-     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi error, omnis similique sequi, itaque ullam blanditiis id earum hic laboriosam reprehenderit ducimus consequuntur veritatis totam quisquam illo dignissimos mollitia. Est.
-     
-    </div>
+    
     <div className="col mt-5 mb-0 pt-5 container">
    <img src={hero} alt="hero" width="735rem" height='433rem' className='img-fluid' />
     </div>
