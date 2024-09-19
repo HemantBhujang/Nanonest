37354524a74dda648f0ef71c6e6ebe1738aafedb
sakshi Kakde <sakshi.kakde2004@gmail.com>
2024-09-19 20:55:14 +0530

    redirect done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
-import Navbar from './component/Navbar'
-import HeroSection from './component/HeroSection'
-import Section2 from './component/Section2'
-import Apointment from './component/Apointment'
-import Testimonials from './component/Testimonials'
-import Services from './component/Services'
-// import worker from '..assets/Worker.png' //it give me error that can't import
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './component/Navbar';
+import HeroSection from './component/HeroSection';
+import Section2 from './component/Section2';
+import Appointment from './component/Apointment';
+import Testimonials from './component/Testimonials';
+import Services from './component/Services';
 
 const App = () => {
   return (
-    <div>
-      <Navbar title='NanoNest' services='Services' industry='Industry' about='About Us'  team ='Team NanoNest' more='More' faq='Founder FAQ' in_faq='Investor FAQ' blog='Blog'/>
-      <HeroSection/>
-      <Section2 />
-      <Apointment />
-      <Testimonials />
-      <Services title='Services'  content='All our digital marketing services start with a free 30-minute consultation call where we find the best strategy towards reaching your goals.'/>
-    </div>
-  )
+    <Router>
+      <div>
+        <Navbar title='NanoNest' services='Services' industry='Industry' about='About Us' team ='Team NanoNest' more='More' faq='Founder FAQ' in_faq='Investor FAQ' blog='Blog'/>
+        
+        {/* Define routes */}
+        <Routes>
+          {/* Home route with multiple components */}
+          <Route path="/" element={
+            <>
+              <HeroSection />
+              <Section2 />
+              <Appointment />
+              <Testimonials />
+            </>
+          }/>
+          
+          {/* Services route */}
+          <Route path="/services" element={
+            <Services 
+              title='Services'  
+              content='All our digital marketing services start with a free 30-minute consultation call where we find the best strategy towards reaching your goals.'
+            />
+          } />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,54 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from "../assets/react.svg";
 
-
-
-const Navbar = ({title,services,industry,about,team,more,faq,in_faq,blog}) => {
+const Navbar = ({ title, services, industry, about, team, more, faq, in_faq, blog }) => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary shadow p-2 mb-4 bg-white rounded">
-  <div className="container-fluid">
-  <a className="navbar-brand" href="#">
- 
-      <img src={logo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top mx-2"></img>
-      {title}
-    </a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active mx-3" aria-current="page" href="#">{services}</a>
-        </li>
-        <li className="nav-item">
-      <a className="nav-link active mx-3 " aria-current="page" href="#">{industry}</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active mx-3" aria-current="page" href="#">{about}</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link active mx-3" aria-current="page" href="#">{team}</a>
-        </li>
-        <li className="nav-item dropdown mx-3">
-          <a className="nav-link dropdown-toggle mx-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            {more}
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">{faq}</a></li>
-            <li><a className="dropdown-item" href="#">{in_faq}</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">{blog}</a></li>
-          </ul>
-        </li>
-    </ul>
-
-    <button type="button" class="btn btn-outline-warning mx-3">Contact Us </button>
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <img src={logo} alt="Logo" width="30" height="24" className="d-inline-block align-text-top mx-2"></img>
+            {title}
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active mx-3" to="/services">{services}</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active mx-3" to="/industry">{industry}</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active mx-3" to="/about">{about}</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active mx-3" to="/team">{team}</Link>
+              </li>
+              <li className="nav-item dropdown mx-3">
+                <a className="nav-link dropdown-toggle mx-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  {more}
+                </a>
+                <ul className="dropdown-menu">
+                  <li><Link className="dropdown-item" to="/faq">{faq}</Link></li>
+                  <li><Link className="dropdown-item" to="/investor-faq">{in_faq}</Link></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><Link className="dropdown-item" to="/blog">{blog}</Link></li>
+                </ul>
+              </li>
+            </ul>
+            <button type="button" className="btn btn-outline-warning mx-3">Contact Us</button>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-    </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
