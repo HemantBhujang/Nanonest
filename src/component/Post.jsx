c1eff60e71c4d859c6464a098ef463055b2fe69f
sakshi Kakde <sakshi.kakde2004@gmail.com>
2024-09-20 22:09:38 +0530

    ..

--- src/component/Post.jsx
+++ src/component/Post.jsx
@@ -2,19 +2,36 @@ import React from 'react'
 
 const Post = ({ProfilePic,Name,Content,Button}) => {
   return (
-    <div class="container text-center">
-  <div class="row">
-    <div class="col">
-      Column
-    </div>
-    <div class="col">
-      Column
-    </div>
-    <div class="col">
-      Column
-    </div>
-  </div>
-</div>
+    <>
+    <div className="container text-center">
+      <div className='row'>
+              <div className="card col mx-5 my-5 shadow-lg mb-4" style={{width: '18rem', borderColor:'#F9BC6E'}}>
+            <img src={ProfilePic} className="card-img-fluid" alt="Profile Picture"/>
+            <div className="card-body">
+              <h5 className="card-title">{Name}</h5>
+              <p className="card-text">{Content}</p>
+              <a href="#" class="btn btn-warning">{Button}</a>
+            </div>
+          </div>
+          <div className="card col mx-5 my-5 shadow-lg mb-4" style={{width: '18rem', borderColor:'#F9BC6E'}}>
+            <img src={ProfilePic} className="card-img-fluid" alt="Profile Picture"/>
+            <div className="card-body">
+              <h5 className="card-title">{Name}</h5>
+              <p className="card-text">{Content}</p>
+              <a href="#" class="btn btn-warning">{Button}</a>
+            </div>
+            </div>
+            <div className="card col mx-5 my-5 shadow-lg mb-4" style={{width: '18rem',borderColor:'#F9BC6E'}}>
+            <img src={ProfilePic} className="card-img-fluid" alt="Profile Picture"/>
+            <div className="card-body">
+              <h5 className="card-title">{Name}</h5>
+              <p className="card-text">{Content}</p>
+              <a href="#" class="btn btn-warning">{Button}</a>
+            </div>
+            </div>
+            </div>
+        </div>
+    </>
   )
 }
 
