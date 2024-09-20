f067e2ea1aaa37083f4b9eab300c311879afcb08
Hemant Bhujang <mrhemant619@gmail.com>
2024-09-20 20:10:20 +0530

    Dropdown Button and border for the post is added

--- src/component/AfterLogin.jsx
+++ src/component/AfterLogin.jsx
@@ -4,11 +4,22 @@ import Tabish from '../assets/Tabish Khan.jpg'
 
 const AfterLogin = ({title,content,wave}) => {
   return (
-    <>    <div className='container text-start my-5'>
+    <>    
+    <div className='container text-start my-5'>
         <h1 style={{fontSize:'8vmin'}} className='my-3'>{title}</h1>
         <h3 style={{fontSize:'4vmin', color:'#F9BC6E'}}>{content}</h3> 
     </div>
     <img src={wave} alt="Wave Image" className='Start' width='100%' />
+    <div class="btn-group dropend" style={{marginLeft:'90%'}}>
+      <button type="button" class="btn btn-warning dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+        Dropend
+      </button>
+      <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#">Investors</a></li>
+        <li><a class="dropdown-item" href="#">Entrepreneur</a></li>
+        <li><a class="dropdown-item" href="#">Other</a></li>
+      </ul>
+    </div>
     <Post ProfilePic={Tabish}
               Name="Tabish Khan"
               Content="Aura Digital"
--- src/component/Post.jsx
+++ src/component/Post.jsx
@@ -5,28 +5,28 @@ const Post = ({ProfilePic,Name,Content,Button}) => {
     <>
     <div className="container text-center">
       <div className='row'>
-              <div className="card col mx-5 my-5 shadow-lg mb-4" style={{width: '18rem'}}>
+              <div className="card col mx-5 my-5 shadow-lg mb-4" style={{width: '18rem', borderColor:'#F9BC6E'}}>
             <img src={ProfilePic} className="card-img-fluid" alt="Profile Picture"/>
             <div className="card-body">
               <h5 className="card-title">{Name}</h5>
               <p className="card-text">{Content}</p>
-              <a href="#" class="btn btn-primary">{Button}</a>
+              <a href="#" class="btn btn-warning">{Button}</a>
             </div>
           </div>
-          <div className="card col mx-5 my-5 shadow-lg mb-4" style={{width: '18rem'}}>
+          <div className="card col mx-5 my-5 shadow-lg mb-4" style={{width: '18rem', borderColor:'#F9BC6E'}}>
             <img src={ProfilePic} className="card-img-fluid" alt="Profile Picture"/>
             <div className="card-body">
               <h5 className="card-title">{Name}</h5>
               <p className="card-text">{Content}</p>
-              <a href="#" class="btn btn-primary">{Button}</a>
+              <a href="#" class="btn btn-warning">{Button}</a>
             </div>
             </div>
-            <div className="card col mx-5 my-5 shadow-lg mb-4" style={{width: '18rem'}}>
+            <div className="card col mx-5 my-5 shadow-lg mb-4" style={{width: '18rem',borderColor:'#F9BC6E'}}>
             <img src={ProfilePic} className="card-img-fluid" alt="Profile Picture"/>
             <div className="card-body">
               <h5 className="card-title">{Name}</h5>
               <p className="card-text">{Content}</p>
-              <a href="#" class="btn btn-primary">{Button}</a>
+              <a href="#" class="btn btn-warning">{Button}</a>
             </div>
             </div>
             </div>
