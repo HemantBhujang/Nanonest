c6bdabfac7ccea3316908e25721777af6241cd58
Hemant Bhujang <mrhemant619@gmail.com>
2024-10-03 21:10:40 +0530

    Minor

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -10,18 +10,18 @@ const Profile = ({Profile}) => {
                 <div class="col">
                     <button type="button" class="btn btn-outline-warning my-5">Upload Photo</button></div>
                 <div class="col">
-                   <h4 style={{opacity:'50%'}}>Your Name </h4> 
-                   <h4>Sakshi Kakde</h4>
+                   <h6 style={{opacity:'50%'}}>Your Name </h6> 
+                   <h6>Sakshi Kakde</h6>
                 </div>
                 <div class="col"><button type="button" class="btn btn-outline-warning my-5">Edit</button></div>
                     <div class="col">
-                    <h4 style={{opacity:'50%'}}>Contact Information</h4> 
-                    <h4>sakshikakde@nanonest.in</h4>
-                    <h4>+91987654321</h4>
+                    <h6 style={{opacity:'50%'}}>Contact Information</h6> 
+                    <h6>sakshikakde@nanonest.in</h6>
+                    <h6>+91987654321</h6>
                     </div>
                     <div class="col"><button type="button" class="btn btn-outline-warning my-5">Edit</button></div><div class="col">
-                    <h4 style={{opacity:'50%'}}>About</h4> 
-                    <h4>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, doloribus expedita. Explicabo cupiditate est quod reiciendis consequatur distinctio voluptatem illo exercitationem deserunt quaerat eius incidunt, optio veniam natus labore unde.</h4>
+                    <h6 style={{opacity:'50%'}}>About</h6> 
+                    <h6>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius, doloribus expedita. Explicabo cupiditate est quod reiciendis consequatur distinctio voluptatem illo exercitationem deserunt quaerat eius incidunt, optio veniam natus labore unde.</h6>
                     </div>
                     </div>
 
