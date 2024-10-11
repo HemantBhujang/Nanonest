c6c5fa5dd29612b2b35de53ae1e7e9740e5655c5
Hemant Bhujang <mrhemant619@gmail.com>
2024-10-11 14:26:14 +0530

    Personal Chat Section. Offcanvas

--- src/component/MessageSection.jsx
+++ src/component/MessageSection.jsx
@@ -12,7 +12,7 @@ const MessageSection = () => {
           <p className="mb-1">Some placeholder content in a paragraph.</p>
           <small>And some small print.</small>
         </a>
-        <a href="#" className="list-group-item list-group-item-action">
+        <a href="#" className="list-group-item list-group-item-action"  data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
           <div className="d-flex w-100 justify-content-between">
             <h5 className="mb-1">List group item heading</h5>
             <small className="text-body-secondary">3 days ago</small>
@@ -20,7 +20,7 @@ const MessageSection = () => {
           <p className="mb-1">Some placeholder content in a paragraph.</p>
           <small className="text-body-secondary">And some muted small print.</small>
         </a>
-        <a href="#" className="list-group-item list-group-item-action">
+        <a href="#" className="list-group-item list-group-item-action" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
           <div className="d-flex w-100 justify-content-between">
             <h5 className="mb-1">List group item heading</h5>
             <small className="text-body-secondary">3 days ago</small>
@@ -28,6 +28,19 @@ const MessageSection = () => {
           <p className="mb-1">Some placeholder content in a paragraph.</p>
           <small className="text-body-secondary">And some muted small print.</small>
         </a>
+      </div>
+
+      {/* __________________Offcanvas of Personal Message #chat____________ */}
+      <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="staticBackdropLabel">Offcanvas</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <div>
+            I will not close if you click outside of me.
+          </div>
+        </div>
       </div>
   </>
   )
