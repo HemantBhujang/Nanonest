6c6915c828fe154e8a5404f4f609d3a2d1e3dbb5
Hemant Bhujang <mrhemant619@gmail.com>
2025-06-02 15:17:47 +0530

    Hosting Error 2

--- src/component/InvestmentDetail.jsx
+++ src/component/InvestmentDetail.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Navbar2 from './Navbar2';
-import solar from '../images/Solar_Power.jpg';
+import solar from '../Images/Solar_Power.jpg';
 import {
   Box,
   Typography,
